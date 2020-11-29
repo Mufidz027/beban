7d0c88e44f886501019439f9b110b6cfb201b8e3
Mufidz027 <almusyafir240@gmail.com>
2020-11-30 04:43:08 +0700

    Initial commit

---
+++ system/app/Http/Controllers/ClientProdukController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Produk;
+use App\Models\User;
+
+class ClientProdukController extends Controller
+{
+	function index(){
+		$data['list_produk'] = produk::all();
+		return view('customer.home', $data);
+	}
+
+	function showProduk(){
+		$data['show_produk'] = produk::all();
+		return view('customer.produk', $data);
+	}
+
+	function show(Produk $produk){
+		$data['produk'] = $produk;
+		return view('customer.produk_single', $data);
+	}
+
+	function kontak(){
+		return view('customer.kontak');
+	}
+	function filter(){
+		$nama = request('nama');
+		$data['show_produk'] = Produk::where('nama_produk', 'like', "%$nama%")->get();
+		$data['nama'] = $nama;
+		return view('customer.produk', $data);
+	}
+}
